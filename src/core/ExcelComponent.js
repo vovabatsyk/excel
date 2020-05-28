cea7528203d5d6a073ea8a533a69440dfabdfab5
vovabatsyk <31743545+vovabatsyk@users.noreply.github.com>
2020-05-28 16:40:11 +0300

    Finish base table logic (#10)

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,11 +4,24 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.prepare()
+    this.unsubscribers = []
   }
 
-  // Возвращает шаблон компонента
+  prepare() {}
+
   toHTML() {
     return ''
+  }
+
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
   }
 
   init() {
@@ -17,5 +30,6 @@ export class ExcelComponent extends DomListener {
 
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
