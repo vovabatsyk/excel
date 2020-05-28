3a97bd43db40ce153c4a87b299119bf0966929de
Vova Batsyk <vovabatsyk@gmail.com>
2020-05-28 16:37:27 +0300

    Finish base table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,13 +1,18 @@
 import {ExcelComponent} from '@core/ExcelComponent'
-import {createTable} from '@/components/table/table.template'
 import {$} from '@core/dom'
+import {createTable} from '@/components/table/table.template'
+import {resizeHandler} from '@/components/table/table.resize'
+import {isCell, matrix, nextSelector, shouldResize} from './table.functions'
+import {TableSelection} from '@/components/table/TableSelection'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
-      listeners: ['mousedown']
+      name: 'Table',
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options
     })
   }
 
@@ -15,51 +20,65 @@ export class Table extends ExcelComponent {
     return createTable(20)
   }
 
+  prepare() {
+    this.selection = new TableSelection()
+  }
+
+  init() {
+    super.init()
+
+    this.selectCell(this.$root.find('[data-id="0:0"]'))
+
+    this.$on('formula:input', text => {
+      this.selection.current.text(text)
+    })
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus()
+    })
+  }
+
+  selectCell($cell) {
+    this.selection.select($cell)
+    this.$emit('table:select', $cell)
+  }
+
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target)
-      const $parent = $resizer.closest('[data-type="resizable"]')
-      const coords = $parent.getCoords()
-      const type = $resizer.data.resize
-      const sideProp = type === 'col' ? 'bottom' : 'right'
-      let value
-
-      $resizer.css({
-        opacity: 1,
-        [sideProp]: '-5000px'
-      })
-
-      document.onmousemove = e => {
-        if (type === 'col') {
-          const delta = e.pageX - coords.right
-          value = coords.width + delta
-          $resizer.css({right: -delta + 'px'})
-        } else {
-          const delta = e.pageY - coords.bottom
-          value = coords.height + delta
-          $resizer.css({bottom: -delta + 'px'})
-        }
-      }
-
-      document.onmouseup = () => {
-        document.onmousemove = null
-        document.onmouseup = null
-
-        if (type === 'col') {
-          $parent.css({width: value + 'px'})
-          this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-              .forEach(el => el.style.width = value + 'px')
-        } else {
-          $parent.css({height: value + 'px'})
-        }
-
-        $resizer.css({
-          opacity: 0,
-          bottom: 0,
-          right: 0
-        })
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
+    } else if (isCell(event)) {
+      const $target = $(event.target)
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current)
+            .map(id => this.$root.find(`[data-id="${id}"]`))
+        this.selection.selectGroup($cells)
+      } else {
+        this.selection.select($target)
       }
     }
   }
+
+  onKeydown(event) {
+    const keys = [
+      'Enter',
+      'Tab',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowDown',
+      'ArrowUp'
+    ]
+
+    const {key} = event
+
+    if (keys.includes(key) && !event.shiftKey) {
+      event.preventDefault()
+      const id = this.selection.current.id(true)
+      const $next = this.$root.find(nextSelector(key, id))
+      this.selectCell($next)
+    }
+  }
+
+  onInput(event) {
+    this.$emit('table:input', $(event.target))
+  }
 }
-  
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,10 +3,18 @@ const CODES = {
   Z: 90
 }
 
-function toCell(_, col) {
-  return `
-    <div class="cell" contenteditable data-col="${col}"></div>
-  `
+function toCell(row) {
+  return function(_, col) {
+    return `
+      <div 
+        class="cell" 
+        contenteditable 
+        data-col="${col}"
+        data-type="cell"
+        data-id="${row}:${col}"
+      ></div>
+    `
+  }
 }
 
 function toColumn(col, index) {
@@ -36,7 +44,7 @@ function toChar(_, index) {
 }
 
 export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
+  const colsCount = CODES.Z - CODES.A + 1
   const rows = []
 
   const cols = new Array(colsCount)
@@ -47,13 +55,13 @@ export function createTable(rowsCount = 15) {
 
   rows.push(createRow(null, cols))
 
-  for (let i = 0; i < rowsCount; i++) {
+  for (let row = 0; row < rowsCount; row++) {
     const cells = new Array(colsCount)
         .fill('')
-        .map(toCell)
+        .map(toCell(row))
         .join('')
 
-    rows.push(createRow(i + 1, cells))
+    rows.push(createRow(row + 1, cells))
   }
 
   return rows.join('')
