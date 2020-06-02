26f068a4bcf02683e79fa1e6d5c7af45c7bfed1d
Vova Batsyk <vovabatsyk@gmail.com>
2020-06-02 17:18:32 +0300

    localStorage

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,47 @@
+import { DomListener } from '@core/DomListener'
+
+export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+    this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
+    this.prepare()
+    this.unsubscribers = []
+    this.storeSub = null
+  }
+
+  prepare() {}
+
+  toHTML() {
+    return ''
+  }
+
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  $on(event, fn) {
+    this.emitter.subscribe(event, fn)
+  }
+
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
+  }
+
+  init() {
+    this.initDOMListeners()
+  }
+
+  destroy() {
+    this.removeDOMListeners()
+    this.unsubscribers.forEach((unsub) => unsub())
+  }
+}
